af7af605ba6fd8a1aa88ff17cc1832b1bacd1416
Aleksei Matiushkin <am@mudasobwa.ru>
2018-01-07 22:15:01 +0100

    Refactored direct lookup.

--- lib/lib_lat_lon/providers/open_street_map.ex
+++ lib/lib_lat_lon/providers/open_street_map.ex
@@ -5,35 +5,57 @@ defmodule LibLatLon.Providers.OpenStreetMap do
 
   @server_url "https://nominatim.openstreetmap.org"
   @search Enum.join([@server_url, "search"], "/")
-  @reverse Enum.join([@server_url, "reverse.php"], "/")
+  @reverse Enum.join([@server_url, "reverse"], "/")
 
-  @defaults %{
-    format: :json,
-    zoom: 16,
-    addressdetails: 1
-  }
+  @defaults %{ "accept-language": "en" }
+  @reverse_defaults %{ format: :json, zoom: 16, addressdetails: 1 }
+  @search_defaults %{ format: :json, polygon_geojson: 1, viewbox: "" }
 
   def name(), do: "Open Street Map"
 
-  # "https://nominatim.openstreetmap.org/reverse?format=json&
+  def lookup(input, opts \\ @defaults)
+
+    # "https://nominatim.openstreetmap.org/reverse?format=json&
   #           accept-language={{ language }}&lat={{ latitude }}&
   #           lon={{ longitude }}&zoom={{ zoom }}&addressdetails=1"
-  def lookup(%LibLatLon.Coords{lat: lat, lon: lon}, opts \\ @defaults) do
+  def lookup(%LibLatLon.Coords{lat: lat, lon: lon}, opts) do
     query =
       opts
+      |> Map.merge(@reverse_defaults)
       |> Map.merge(%{lat: lat, lon: lon})
-      |> Enum.map(fn {k, v} -> "#{k}=#{v}" end)
-      |> Enum.join("&")
 
-    case HTTPoison.get(Enum.join([@reverse, query], "?")) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        Jason.decode!(body)
+    [@reverse, URI.encode_query(query)]
+    |> Enum.join("?")
+    |> do_lookup()
+  end
 
+  # # "https://nominatim.openstreetmap.org/search?format=json
+  #             q=Barcelona+c%2Fde+Marina+16&polygon_geojson=1&viewbox=
+  def lookup(address, opts) when is_binary(address) do
+    query =
+      opts
+      |> Map.merge(@search_defaults)
+      |> Map.merge(%{q: address})
+      |> URI.encode_query()
+
+    [@search, query]
+    |> Enum.join("?")
+    |> do_lookup()
+  end
+
+  defp do_lookup(query) do
+    with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- HTTPoison.get(query),
+         {:ok, result} <- Jason.decode(body) do
+      result
+    else # FIXME BETTER ERROR HANDLING
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts("Not found :(")
+        {:error, :not_found}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason)
+        {:error, reason}
+
+      {:error, jason_error} ->
+        {:error, jason_error}
     end
   end
 end
